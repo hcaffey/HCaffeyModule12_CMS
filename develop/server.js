71c487f6095a8aa853fdaa33bdeece239b3cf630
Hayden Caffey <hayden@Haydens-MacBook-Pro-2.local>
2023-08-03 18:01:51 -0400

    current

--- develop/server.js
+++ develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,9 +14,7 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
     user: 'root',
-    // TODO: Add MySQL password here
     password: 'password',
     database: 'employee_db'
   },
@@ -23,9 +22,66 @@ const db = mysql.createConnection(
 );
 
 
+const prompts = [
+  {
+    type: 'list',
+    name: 'title',
+    message: 'What would you like to view?:',
+    choices: ['view departments', 'view roles', 'view employees', 'add department', ]
+  },
+];
 
+function init() {
+  inquirer.prompt(prompts).then((data) => {
+    switch (data.title) {
+      case 'view departments':
+        viewDepartments();
+        break;
+      case 'view roles':
+        viewRoles();
+        break;
+      case 'view employees':
+        viewEmployees();
+        break;
+      case 'add department':
+        addDepartment();
+        break;
+    }
+  });
+}
 
+function viewDepartments() {
+  db.query('SELECT * FROM department', (err, results) => {
+    if (err) {
+      console.error('Error fetching departments: ', err);
+      return;
+    }
+    console.table(results);
+    init(); // Prompt for action again
+  });
+}
 
+function viewRoles() {
+  db.query('SELECT * FROM role', (err, results) => {
+    if (err) {
+      console.error('Error fetching roles: ', err);
+      return;
+    }
+    console.table(results);
+    init(); // Prompt for action again
+  });
+}
+
+function viewEmployees() {
+  db.query('SELECT * FROM employee', (err, results) => {
+    if (err) {
+      console.error('Error fetching employees: ', err);
+      return;
+    }
+    console.table(results);
+    init(); // Prompt for action again
+  });
+}
 
 
 app.use((req, res) => {
@@ -35,3 +91,5 @@ app.use((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+init();
